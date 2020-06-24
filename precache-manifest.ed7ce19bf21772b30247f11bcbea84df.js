049499b227e727228db6fed7ec05c15fb3ced7da
sahil1693 <sahil171693.cse@chitkara.edu.in>
2020-06-24 09:42:15 +0530

    Updates

---
+++ precache-manifest.ed7ce19bf21772b30247f11bcbea84df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344e2c457b043a3106da24299832ec39",
+    "url": "/Urlshortner/index.html"
+  },
+  {
+    "revision": "29ae160beb404f2934ed",
+    "url": "/Urlshortner/static/css/main.ccd56ed3.chunk.css"
+  },
+  {
+    "revision": "e035c979540fd9065b4f",
+    "url": "/Urlshortner/static/js/2.7ab7e226.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "/Urlshortner/static/js/2.7ab7e226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ae160beb404f2934ed",
+    "url": "/Urlshortner/static/js/main.1093bc77.chunk.js"
+  },
+  {
+    "revision": "b293af1ed4b65220e700",
+    "url": "/Urlshortner/static/js/runtime-main.80c9a675.js"
+  },
+  {
+    "revision": "0a961495858b113475f1a8e2af9bcdb5",
+    "url": "/Urlshortner/static/media/background.0a961495.jpg"
+  }
+]);
